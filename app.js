75280b4394d609b4502a3b8eb553b743b030d8b6
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-05-01 23:05:24 +0500

    react

---
+++ app.js
@@ -0,0 +1,39 @@
+class HelloMessage extends React.Component {
+    render() {
+      return <div>Hello {this.props.name}</div>;
+    }
+  }
+  
+  root.render(<HelloMessage name="Taylor" />);
+
+
+  class Timer extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { seconds: 0 };
+    }
+  
+    tick() {
+      this.setState(state => ({
+        seconds: state.seconds + 1
+      }));
+    }
+  
+    componentDidMount() {
+      this.interval = setInterval(() => this.tick(), 1000);
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+  
+    render() {
+      return (
+        <div>
+          Seconds: {this.state.seconds}
+        </div>
+      );
+    }
+  }
+  
+  root.render(<Timer />);
